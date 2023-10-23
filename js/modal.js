6cc12d0b42c05134b613f0b2bdf6dda47bf84176
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-10-23 23:10:26 +0300

    коррекция swiper

--- js/modal.js
+++ js/modal.js
@@ -169,10 +169,12 @@ const productDetail = document.querySelector('.product_detail');
 
 productDetail.addEventListener('click', () => {
   const slides = [
-    { imageSrc: '../images/odejda_1-1.jpg', text: 'Текст для первой картинки' },
-    { imageSrc: '../images/odejda_1-2.jpg', text: 'Текст для второй картинки' },
-    { imageSrc: '../images/odejda_1-3.jpg', text: 'Текст для третьей картинки' },
-    // Добавьте свои данные для слайдов здесь
+    { imageSrc: '../images/odejda_1-1.jpg', text: 'Текст для картинки 1' },
+    { imageSrc: '../images/odejda_1-2.jpg', text: 'Текст для картинки 2' },
+    { imageSrc: '../images/odejda_1-3.jpg', text: 'Текст для картинки 3' },
+    { imageSrc: '../images/odejda_1-4.jpg', text: 'Текст для картинки 4' },
+    { imageSrc: '../images/odejda_1-5.jpg', text: 'Текст для картинки 5' },
+    { imageSrc: '../images/odejda_1-6.jpg', text: 'Текст для картинки 6' },
   ];
 
   createModalWithSlides(slides);
