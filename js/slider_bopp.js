df590e442927533a81c9e14c25d5ab2913ea2f11
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-11-05 20:57:28 +0200

    картинки

--- js/slider_bopp.js
+++ js/slider_bopp.js
@@ -5,9 +5,11 @@ const productDetail_1 = document.querySelector('.product_detail_1');
 
 productDetail_1.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/bopp_1.jpg', text: 'Текст для картинки 2' },
-    { imageSrc: '../images/bopp_2.jpg', text: 'Текст для картинки 1' },
-    { imageSrc: '../images/bopp_3.jpg', text: 'Текст для картинки 3' },
+    { imageSrc: '../images/bopp_1-1.jpg', text: 'Текст для картинки 1' },
+    { imageSrc: '../images/bopp_1-2.jpg', text: 'Текст для картинки 2' },
+    { imageSrc: '../images/bopp_1-3.jpg', text: 'Текст для картинки 3' },
+    { imageSrc: '../images/bopp_1-4.jpg', text: 'Текст для картинки 4' },
+    { imageSrc: '../images/bopp_1-5.jpg', text: 'Текст для картинки 5' },
   ];
 
   createImageSlider(images);
@@ -17,9 +19,10 @@ const productDetail_2 = document.querySelector('.product_detail_2');
 
 productDetail_2.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/bopp_1.jpg', text: 'Текст для картинки 2' },
-    { imageSrc: '../images/bopp_2.jpg', text: 'Текст для картинки 1' },
-    { imageSrc: '../images/bopp_3.jpg', text: 'Текст для картинки 3' },
+    { imageSrc: '../images/bopp_2-1.jpg', text: 'Текст для картинки 1' },
+    { imageSrc: '../images/bopp_2-2.jpg', text: 'Текст для картинки 2' },
+    { imageSrc: '../images/bopp_2-3.jpg', text: 'Текст для картинки 3' },
+    { imageSrc: '../images/bopp_2-4.jpg', text: 'Текст для картинки 4' },
   ];
 
   createImageSlider(images);
