d389bb1af7dceb5eb0b823fa229be18da4071f46
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-11-10 20:55:39 +0200

    картинки в слайдере подключил

--- js/slider_bopp.js
+++ js/slider_bopp.js
@@ -10,6 +10,10 @@ productDetail_1.addEventListener('click', () => {
     { imageSrc: '../images/bopp_1-3.jpg', text: "Текст для картинки 3" },
     { imageSrc: '../images/bopp_1-4.jpg', text: "Текст для картинки 4" },
     { imageSrc: '../images/bopp_1-5.jpg', text: "Текст для картинки 5" },
+    { imageSrc: '../images/bopp_1-6.jpg', text: "Текст для картинки 6" },
+    { imageSrc: '../images/bopp_1-7.jpg', text: "Текст для картинки 7" },
+    { imageSrc: '../images/bopp_1-8.jpg', text: "Текст для картинки 8" },
+    { imageSrc: '../images/bopp_1-9.jpg', text: "Текст для картинки 9" },
   ];
 
   createImageSlider(images);
@@ -19,11 +23,11 @@ const productDetail_2 = document.querySelector('.product_detail_2');
 
 productDetail_2.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/bopp_2-1.jpg', text: 'Текст для картинки 1' },
-    { imageSrc: '../images/bopp_2-2.jpg', text: 'Текст для картинки 2' },
-    { imageSrc: '../images/bopp_2-3.jpg', text: 'Текст для картинки 3' },
-    { imageSrc: '../images/bopp_2-4.jpg', text: 'Текст для картинки 4' },
-    { imageSrc: '../images/bopp_2-5.jpg', text: 'Текст для картинки 5' },
+    { imageSrc: '../images/bopp_2-1.jpg', text: "Текст для картинки 1" },
+    { imageSrc: '../images/bopp_2-2.jpg', text: "Текст для картинки 2" },
+    { imageSrc: '../images/bopp_2-3.jpg', text: "Текст для картинки 3" },
+    { imageSrc: '../images/bopp_2-4.jpg', text: "Текст для картинки 4" },
+    { imageSrc: '../images/bopp_2-5.jpg', text: "Текст для картинки 5" },
   ];
 
   createImageSlider(images);
--- js/slider_musor.js
+++ js/slider_musor.js
@@ -5,24 +5,15 @@ const productDetail_1 = document.querySelector('.product_detail_1');
 
 productDetail_1.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/musor_1.jpg', text: "Текст для картинки 1" },
-    { imageSrc: '../images/musor.jpg', text: "Текст для картинки 2" },
-    { imageSrc: '../images/musor_1.jpg', text: "Текст для картинки 3" },
+    { imageSrc: '../images/musor_1-1.jpg', text: '30 літрів' },
+    { imageSrc: '../images/musor_1-2.jpg', text: '60 лiтрiв' },
+    { imageSrc: '../images/musor_1-3.jpg', text: '120 лiтрiв' },
+    { imageSrc: '../images/musor_1-4.jpg', text: '240 лiтрiв' },
+    { imageSrc: '../images/musor_1-5.jpg', text: 'Рулон віддільних пакетів' },
   ];
 
   createImageSlider(images);
 });
-
-// const productDetail_2 = document.querySelector('.product_detail_2');
-
-// productDetail_2.addEventListener('click', () => {
-//   const images = [
-//     { imageSrc: '../images/musor_1.jpg', text: 'Текст для картинки 1' },
-//     { imageSrc: '../images/musor_1.jpg', text: 'Текст для картинки 2' },
-//   ];
-
-//   createImageSlider(images);
-// });
 
 window.addEventListener('load', () => {
   closeModalFunction(document.getElementById('modalProduct'));
