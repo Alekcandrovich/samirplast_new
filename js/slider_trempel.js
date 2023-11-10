f5a8cc8b7f1737cded3165eb93a6b9852135a353
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-11-10 18:57:40 +0200

    перевод

--- js/slider_trempel.js
+++ js/slider_trempel.js
@@ -5,10 +5,10 @@ const productDetail_1 = document.querySelector('.product_detail_1');
 
 productDetail_1.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/trempel_1-1.jpg', text: 'Текст для картинки 1' },
-    { imageSrc: '../images/trempel_1-2.jpg', text: 'Текст для картинки 2' },
-    { imageSrc: '../images/trempel_1-3.jpg', text: 'Текст для картинки 3' },
-    { imageSrc: '../images/trempel_1-4.jpg', text: 'Текст для картинки 4' },
+    { imageSrc: '../images/trempel_1-1.jpg', text: 'пластмасовий дитячий (32см.)' },
+    { imageSrc: '../images/trempel_1-2.jpg', text: 'пластмасовий (літо, 38см.)' },
+    { imageSrc: '../images/trempel_1-3.jpg', text: 'пластмасовий (весна, 40см.)' },
+    { imageSrc: '../images/trempel_1-4.jpg', text: 'пластмасовий (зима, 40см.)' },
   ];
 
   createImageSlider(images);
