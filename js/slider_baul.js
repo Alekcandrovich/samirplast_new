2e116056b5b6cf36cd7d3ede39aa4371b6908d86
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-11-15 18:52:17 +0200

    переименование картинок

---
+++ js/slider_baul.js
@@ -0,0 +1,30 @@
+import createImageSlider from './modal_product.js';
+import { closeModalFunction } from './modal_product.js';
+
+const productDetail_1 = document.querySelector('.product_detail_1');
+
+productDetail_1.addEventListener('click', () => {
+  const images = [
+    { imageSrc: '../images/meshki_1-1.jpg', text: 'Текст для картинки 1' },
+    { imageSrc: '../images/meshki_1-2.jpg', text: 'Текст для картинки 2' },
+    { imageSrc: '../images/meshki_1-3.jpg', text: 'Текст для картинки 3' },
+    { imageSrc: '../images/meshki_1-4.jpg', text: 'Текст для картинки 4' },
+  ];
+
+  createImageSlider(images);
+});
+
+// const productDetail_2 = document.querySelector('.product_detail_2');
+
+// productDetail_2.addEventListener('click', () => {
+//   const images = [
+//     { imageSrc: '../images/meshki_1.jpg', text: 'Текст для картинки 1' },
+//     { imageSrc: '../images/meshki_2.jpg', text: 'Текст для картинки 2' },
+//   ];
+
+//   createImageSlider(images);
+// });
+
+window.addEventListener('load', () => {
+  closeModalFunction(document.getElementById('modalProduct'));
+});
--- js/slider_meshki.js
+++ js/slider_meshki.js
@@ -5,25 +5,27 @@ const productDetail_1 = document.querySelector('.product_detail_1');
 
 productDetail_1.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/meshki_1-1.jpg', text: 'Текст для картинки 1' },
-    { imageSrc: '../images/meshki_1-2.jpg', text: 'Текст для картинки 2' },
-    { imageSrc: '../images/meshki_1-3.jpg', text: 'Текст для картинки 3' },
-    { imageSrc: '../images/meshki_1-4.jpg', text: 'Текст для картинки 4' },
+    { imageSrc: '../images/no_image_400.jpg', text: 'Текст для картинки 1' },
+    { imageSrc: '../images/no_image_400.jpg', text: 'Текст для картинки 2' },
+    { imageSrc: '../images/no_photo_400.jpg', text: 'Текст для картинки 3' },
   ];
 
   createImageSlider(images);
 });
 
-// const productDetail_2 = document.querySelector('.product_detail_2');
+const productDetail_2 = document.querySelector('.product_detail_2');
 
-// productDetail_2.addEventListener('click', () => {
-//   const images = [
-//     { imageSrc: '../images/meshki_1.jpg', text: 'Текст для картинки 1' },
-//     { imageSrc: '../images/meshki_2.jpg', text: 'Текст для картинки 2' },
-//   ];
+productDetail_2.addEventListener('click', () => {
+  const images = [
+    { imageSrc: '../images/musor_1-1.jpg', text: '30 літрів' },
+    { imageSrc: '../images/musor_1-2.jpg', text: '60 лiтрiв' },
+    { imageSrc: '../images/musor_1-3.jpg', text: '120 лiтрiв' },
+    { imageSrc: '../images/musor_1-4.jpg', text: '240 лiтрiв' },
+    { imageSrc: '../images/musor_1-5.jpg', text: 'Рулон віддільних пакетів' },
+  ];
 
-//   createImageSlider(images);
-// });
+  createImageSlider(images);
+});
 
 window.addEventListener('load', () => {
   closeModalFunction(document.getElementById('modalProduct'));
