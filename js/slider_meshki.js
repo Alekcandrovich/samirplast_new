424163ec80345358ef2aba60227d1ae0c56a6cb9
Alekcandrovich <ravara128@gmail.com>
2023-11-23 15:00:23 +0200

    картинки для meshki

--- js/slider_meshki.js
+++ js/slider_meshki.js
@@ -5,9 +5,9 @@ const productDetail_1 = document.querySelector('.product_detail_1');
 
 productDetail_1.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/no_image_2.jpg', text: 'Текст для картинки 1' },
-    { imageSrc: '../images/no_image_2.jpg', text: 'Текст для картинки 2' },
-    { imageSrc: '../images/no_image_2.jpg', text: 'Текст для картинки 3' },
+    { imageSrc: '../images//meshki_1-1.jpg', text: 'Текст для картинки 1' },
+    { imageSrc: '../images//meshki_1-2.jpg', text: 'Текст для картинки 2' },
+    { imageSrc: '../images//meshki_1-3.jpg', text: 'Текст для картинки 3' },
   ];
 
   createImageSlider(images);
@@ -17,11 +17,11 @@ const productDetail_2 = document.querySelector('.product_detail_2');
 
 productDetail_2.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/meshki_1-1.jpg', text: '30 літрів' },
-    { imageSrc: '../images/meshki_1-2.jpg', text: '60 лiтрiв' },
-    { imageSrc: '../images/meshki_1-3.jpg', text: '120 лiтрiв' },
-    { imageSrc: '../images/meshki_1-4.jpg', text: '240 лiтрiв' },
-    { imageSrc: '../images/meshki_1-5.jpg', text: 'Рулон віддільних пакетів' },
+    { imageSrc: '../images/meshki_2-1.jpg', text: '30 літрів' },
+    { imageSrc: '../images/meshki_2-2.jpg', text: '60 лiтрiв' },
+    { imageSrc: '../images/meshki_2-3.jpg', text: '120 лiтрiв' },
+    { imageSrc: '../images/meshki_2-4.jpg', text: '240 лiтрiв' },
+    { imageSrc: '../images/meshki_2-5.jpg', text: 'Рулон віддільних пакетів' },
   ];
 
   createImageSlider(images);
