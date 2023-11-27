f8490f6c041943bcd390eea72032194d0ffa3b4d
Alekcandrovich <ravara128@gmail.com>
2023-11-27 15:38:40 +0200

    картинки и текс в слайдере для mayka

--- js/slider_mayka.js
+++ js/slider_mayka.js
@@ -5,10 +5,11 @@ const productDetail_1 = document.querySelector('.product_detail_1');
 
 productDetail_1.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/mayka_1-1.jpg', text: 'Текст для картинки 1' },
-    { imageSrc: '../images/mayka_1-2.jpg', text: 'Текст для картинки 2' },
-    { imageSrc: '../images/mayka_1-3.jpg', text: 'Текст для картинки 3' },
-    { imageSrc: '../images/mayka_1-4.jpg', text: 'Текст для картинки 4' },
+    { imageSrc: '../images/mayka_1-1.jpg', text: 'Майка  у синю смужку (44+8х2)х70/15' },
+    { imageSrc: '../images/mayka_1-2.jpg', text: 'Майка чорна «Saeedoff» (38+8х2)х60/15' },
+    { imageSrc: '../images/mayka_1-3.jpg', text: 'Майка «Бджілка» (28+8х2)х47/15' },
+    { imageSrc: '../images/mayka_1-4.jpg', text: 'Майка «Козак» (22+8х2)х43/15' },
+    { imageSrc: '../images/mayka_1-5.jpg', text: 'Майка «Luxe» 200 шт (24х42)' },
   ];
 
 
