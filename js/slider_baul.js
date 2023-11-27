1cffed17b8b7828b8c1343dd9ecad050be9b75ba
Alekcandrovich <ravara128@gmail.com>
2023-11-27 15:41:57 +0200

    картинка в слайдер для baul

--- js/slider_baul.js
+++ js/slider_baul.js
@@ -18,8 +18,7 @@ const productDetail_2 = document.querySelector('.product_detail_2');
 
 productDetail_2.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/no_image_2.jpg', text: 'Текст для картинки 1' },
-    { imageSrc: '../images/no_image_2.jpg', text: 'Текст для картинки 2' },
+    { imageSrc: '../images/baul_2-1.jpg', text: 'Текст для картинки 1' },
     { imageSrc: '../images/no_image_2.jpg', text: 'Текст для картинки 2' },
   ];
 
