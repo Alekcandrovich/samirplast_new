6f94e7ba93b1ea8c730f63befb698a2aef632d00
Alekcandrovich <ravara128@gmail.com>
2023-11-29 21:44:53 +0200

    вставил картинки для подкатегорий

--- js/slider_odejda.js
+++ js/slider_odejda.js
@@ -10,18 +10,17 @@ productDetail_1.addEventListener('click', () => {
     { imageSrc: '../images/odejda_1-3.jpg', text: 'Слайд 3 з 3 (Чохли для одягу)' },
   ];
 
-
-createImageSlider(images);
+  createImageSlider(images);
 });
 
 const productDetail_2 = document.querySelector('.product_detail_2');
 
 productDetail_2.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/odejda_2-1.jpg', text: 'Слайд 1 з 2 (Чохли для одягу)' },
-    { imageSrc: '../images/odejda_2-2.jpg', text: 'Слайд 2 з 2 (Чохли для одягу)' },
+    { imageSrc: '../images/odejda_2-1.jpg', text: 'Слайд 1 з 3 (Чохли для одягу)' },
+    { imageSrc: '../images/odejda_2-2.jpg', text: 'Слайд 2 з 3 (Чохли для одягу)' },
+    { imageSrc: '../images/odejda_2-3.jpg', text: 'Слайд 3 з 3 (Чохли для одягу)' },
   ];
-
 
   createImageSlider(images);
 });
