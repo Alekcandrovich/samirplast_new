ea73a13f1568e91f4a2e93429943348f5e6c724d
Alekcandrovich <ravara128@gmail.com>
2023-11-29 22:47:03 +0200

    слайдер для mayka

--- js/slider_mayka.js
+++ js/slider_mayka.js
@@ -6,24 +6,27 @@ const productDetail_1 = document.querySelector('.product_detail_1');
 productDetail_1.addEventListener('click', () => {
   const images = [
     { imageSrc: '../images/mayka_1-1.jpg', text: 'Слайд 1 з 5 Майка у синю смужку (44+8х2)х70/15' },
-    { imageSrc: '../images/mayka_1-2.jpg', text: 'Слайд 2 з 5 Майка чорна «Saeedoff» (38+8х2)х60/15' },
+    {
+      imageSrc: '../images/mayka_1-2.jpg',
+      text: 'Слайд 2 з 5 Майка чорна «Saeedoff» (38+8х2)х60/15',
+    },
     { imageSrc: '../images/mayka_1-3.jpg', text: 'Слайд 3 з 5 Майка «Бджілка» (28+8х2)х47/15' },
     { imageSrc: '../images/mayka_1-4.jpg', text: 'Слайд 4 з 5 Майка «Козак» (22+8х2)х43/15' },
     { imageSrc: '../images/mayka_1-5.jpg', text: 'Слайд 5 з 5 Майка «Luxe» 200 шт (24х42)' },
   ];
 
-
-createImageSlider(images);
+  createImageSlider(images);
 });
 
 const productDetail_2 = document.querySelector('.product_detail_2');
 
 productDetail_2.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/mayka_2-1.jpg', text: 'Слайд 1 з 2 (Пакети типу - «майка»)' },
-    { imageSrc: '../images/mayka_2-2.jpg', text: 'Слайд 2 з 2 (Пакети типу - «майка»)' },
+    { imageSrc: '../images/mayka_2-1.jpg', text: 'Слайд 1 з 4 (Пакети типу - «майка»)' },
+    { imageSrc: '../images/mayka_2-2.jpg', text: 'Слайд 2 з 4 (Пакети типу - «майка»)' },
+    { imageSrc: '../images/mayka_2-3.jpg', text: 'Слайд 3 з 4 (Пакети типу - «майка»)' },
+    { imageSrc: '../images/mayka_2-4.jpg', text: 'Слайд 4 з 4 (Пакети типу - «майка»)' },
   ];
-
 
   createImageSlider(images);
 });
