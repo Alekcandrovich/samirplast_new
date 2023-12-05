a82a8e7c0ad34ac46a876f27442a1f5cbc0587bb
Alekcandrovich <ravara128@gmail.com>
2023-12-05 22:31:47 +0200

    redact

--- js/slider_paket.js
+++ js/slider_paket.js
@@ -5,10 +5,9 @@ const productDetail_1 = document.querySelector('.product_detail_1');
 
 productDetail_1.addEventListener('click', () => {
   const images = [
-    { imageSrc: '../images/paket_1-1.jpg', text: 'Слайд 1 з 4 (Пакети з поліетиленової плівки)' },
-    { imageSrc: '../images/paket_1-2.jpg', text: 'Слайд 2 з 4 (Пакети з поліетиленової плівки)' },
-    { imageSrc: '../images/paket_1-3.jpg', text: 'Слайд 3 з 4 (Пакети з поліетиленової плівки)' },
-    { imageSrc: '../images/paket_1-4.jpg', text: 'Слайд 4 з 4 (Пакети з поліетиленової плівки)' },
+    { imageSrc: '../images/paket_1-1.jpg', text: 'Слайд 1 з 3 (Пакети з поліетиленової плівки)' },
+    { imageSrc: '../images/paket_1-2.jpg', text: 'Слайд 2 з 3 (Пакети з поліетиленової плівки)' },
+    { imageSrc: '../images/paket_1-3.jpg', text: 'Слайд 3 з 3 (Пакети з поліетиленової плівки)' },
   ];
 
   createImageSlider(images);
